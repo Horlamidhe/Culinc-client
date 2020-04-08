a6f6eb383b9f61c8b7897f688cb55ffc2fc5fd13
Horlamidhe <danielolajubu@gmail.com>
2020-04-08 01:24:48 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import { useQuery,useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
